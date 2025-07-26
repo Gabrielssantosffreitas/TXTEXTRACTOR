e630fc41f9f93456a25ae43b5230f16b67e2c14f
Gabriel <gabrielsantos022freitas@gmail.com>
2025-07-26 13:49:28 -0300

    novas Cores

--- src/views/PrintlnCorlor/HeaderColors.java
+++ src/views/PrintlnCorlor/HeaderColors.java
@@ -1,8 +1,29 @@
 package views.PrintlnCorlor;
 
 public class HeaderColors {
-    public static void header(String texto){
+    private static String transformeText(String texto) {
         texto = texto.toUpperCase();
-        PrintlnColors.printColor("==================| "+texto+" |==================\n",Colors.RED);
+        return texto;
     }
+
+    public static void headerRed(String texto) {
+        texto = texto.toUpperCase();
+        PrintlnColors.printColorRed("==================| " + texto + " |==================\n");
+    }
+
+    public static void headerYallow(String texto) {
+        texto = texto.toUpperCase();
+        PrintlnColors.printColorYellow("==================| " + texto + " |==================\n");
+    }
+
+    public static void headerBlue(String texto) {
+        texto = texto.toUpperCase();
+        PrintlnColors.printColorBlue("==================| " + texto + " |==================\n");
+    }
+
+    public static void headerGreew(String texto) {
+        texto = texto.toUpperCase();
+        PrintlnColors.printColor("==================| " + texto + " |==================\n");
+    }
+
 }
--- src/views/PrintlnCorlor/PrintlnColors.java
+++ src/views/PrintlnCorlor/PrintlnColors.java
@@ -4,7 +4,23 @@ public class PrintlnColors {
 
 
     public static void printColor(String texto, Colors colors) {
-        System.out.println( colors.getColor() +texto+ Colors.RESET.getColor() );
+        System.out.println(colors.getColor() + texto + Colors.RESET.getColor());
+    }
+
+    public static void printColor(String texto) {
+        System.out.println(Colors.GREEN.getColor() + texto + Colors.RESET.getColor());
+    }
+
+    public static void printColorRed(String texto) {
+        System.out.println(Colors.RED.getColor() + texto + Colors.RESET.getColor());
+    }
+
+    public static void printColorBlue(String texto) {
+        System.out.println(Colors.BLUE.getColor() + texto + Colors.RESET.getColor());
+    }
+
+    public static void printColorYellow(String texto) {
+        System.out.println(Colors.YELLOW.getColor() + texto + Colors.RESET.getColor());
     }
 
 
